7f6bcfd84edd411d3bbc6a0b10f04a4c3bea8c91
Ollie <oliverdanielsmith@hotmail.com>
2021-03-23 13:56:38 +0000

    Added test for basic simulation

---
+++ src/test/int/singleCellDies.test.tsx
@@ -0,0 +1,14 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Board } from '../../components/Board';
+
+test('When a board with a single alive cell is simulated it dies', () => {
+  render(<Board />);
+  const cells = screen.getAllByRole('button', {name: 'cell'})
+  fireEvent.click(cells[0])
+
+  expect(cells[0]).toHaveAttribute('class', 'alive-true');
+
+  const playButton = screen.getByRole('button', {name: 'Next Step'})
+
+  expect(cells[0]).toHaveAttribute('class', 'alive-false');
+});
